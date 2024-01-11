bf5b2ee4b529550779255da6546e2411c8593b3b
xnought <bertuccd@oregonstate.edu>
2024-01-11 13:55:48 -0800

    feat: include forever serving model

--- activity2/app.js
+++ activity2/app.js
@@ -5,6 +5,27 @@ const PORT = 8020;
 const app = express();
 const pool = connectMySQLPool();
 
+app.get("/", (_, response) => {
+	pool.query(`DROP TABLE IF EXISTS diagnostic;`, () => {
+		pool.query(
+			`CREATE TABLE diagnostic (id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);`,
+			() => {
+				pool.query(
+					`INSERT INTO diagnostic (text) VALUES ("MySQL is working!");`,
+					() => {
+						pool.query(`SELECT * FROM diagnostic;`, (_, result) => {
+							response.send(
+								"<h1>MySQL Results:</h1>" +
+									JSON.stringify(result)
+							);
+						});
+					}
+				);
+			}
+		);
+	});
+});
+
 app.listen(PORT, () => {
 	console.log(`Listening on http://localhost:${PORT}`);
 });
